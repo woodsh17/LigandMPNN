9940c18e44a8b0deb4de597e4c20a681d9ddc5bd
woodsh17 <hmwoods14@gmail.com>
2025-07-24 14:10:28 -0500

    Fixing typo in get_model_params and adding test for all hypermpnn weights

--- test/integration/test_hyper.py
+++ test/integration/test_hyper.py
@@ -28,3 +28,78 @@ def test_run_hyper_for_proteinmpnn():
     print("STDERR:", result.stderr)
     # Check for successful run and expected outputs
     assert result.returncode == 0
+
+
+def test_run_hyper_for_proteinmpnn_e240():
+    cmd = [
+        sys.executable,  # this runs the current Python interpreter
+        "run.py",
+        "--out_folder",
+        "./test/integration/outputs/hyper_e240",
+        "--seed",
+        "111",
+        "--pdb_path",
+        "./test/integration/inputs/1BC8.pdb",
+        "--model_type",
+        "protein_mpnn",
+        "--checkpoint_protein_mpnn",
+        "model_params/hypermpnn_v48_002_epoch240.pt",
+    ]
+
+    result = subprocess.run(
+        cmd, cwd="/Users/woodsh/LigandMPNN", capture_output=True, text=True
+    )
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    # Check for successful run and expected outputs
+    assert result.returncode == 0
+
+
+def test_run_hyper_for_proteinmpnn_01():
+    cmd = [
+        sys.executable,  # this runs the current Python interpreter
+        "run.py",
+        "--out_folder",
+        "./test/integration/outputs/hyper_01",
+        "--seed",
+        "111",
+        "--pdb_path",
+        "./test/integration/inputs/1BC8.pdb",
+        "--model_type",
+        "protein_mpnn",
+        "--checkpoint_protein_mpnn",
+        "model_params/hypermpnn_v48_010_epoch300.pt",
+    ]
+
+    result = subprocess.run(
+        cmd, cwd="/Users/woodsh/LigandMPNN", capture_output=True, text=True
+    )
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    # Check for successful run and expected outputs
+    assert result.returncode == 0
+
+
+def test_run_hyper_for_proteinmpnn_03():
+    cmd = [
+        sys.executable,  # this runs the current Python interpreter
+        "run.py",
+        "--out_folder",
+        "./test/integration/outputs/hyper_03",
+        "--seed",
+        "111",
+        "--pdb_path",
+        "./test/integration/inputs/1BC8.pdb",
+        "--model_type",
+        "protein_mpnn",
+        "--checkpoint_protein_mpnn",
+        "model_params/hypermpnn_v48_030_epoch300.pt",
+    ]
+
+    result = subprocess.run(
+        cmd, cwd="/Users/woodsh/LigandMPNN", capture_output=True, text=True
+    )
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
+    # Check for successful run and expected outputs
+    assert result.returncode == 0
